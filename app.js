d73fe7c721e6fb3dbba932abc449b3113e6b181d
lytranuit <lytranuit@gmail.com>
2018-03-26 18:21:46 +0700

    no message

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const binance = require('node-binance-api');
-
 const mysql = require('promise-mysql');
 const config = require('./config.json');
 const key = require('./key.json');
@@ -67,6 +66,14 @@ var currentTime = null;
  * END CONFIG MAIL
  * 
  *****************/
+
+binance.websockets.prevDay(false, (error, response) => {
+    if (markets[response.symbol]) {
+        markets[response.symbol]['numTrades'] = response.numTrades;
+        markets[response.symbol]['volume'] = response.quoteVolume;
+        markets[response.symbol]['last'] = response.close;
+    }
+});
 binance.prices((error, ticker) => {
     if (error) {
         return console.error(error);
@@ -76,7 +83,10 @@ binance.prices((error, ticker) => {
         var last = ticker[i];
         if (market.indexOf("BTC") != -1) {
             setMarket(market, last);
+            // For a specific symbol:
             binance.websockets.chart(market, "5m", (market, interval, results) => {
+                if (Object.keys(results).length === 0)
+                    return;
 //    let tick = binance.last(chart);
 //    const last = chart[tick].close;
                 var argh = [];
@@ -141,13 +151,12 @@ binance.prices((error, ticker) => {
 //                console.log(results[tick]);
                 if (markets[market].periodTime && markets[market].periodTime == tick && !results[tick].isFinal) {
 //                    console.log(market + " last price: " + last);
-                    markets[market].last = parseFloat(last);
-                    checkPrice(market, last);
+                    markets[market]['chienluoc1'].checkmua(last);
                 } else {
 //                    console.log(tick);
                     if (currentTime != tick) {
                         currentTime = tick;
-                        console.log("Bắt đầu phiên:", moment.unix(tick / 1000).format("YYYY-MM-DD HH:mm:ss.SSS"));
+                        console.log("Bắt đầu phiên:", moment.unix(tick / 1000).format());
                     }
                     if (markets[market]['chienluoc1'].notbuyinsession)
                         markets[market]['chienluoc1'].countIgnoreSession--;
@@ -167,6 +176,18 @@ binance.prices((error, ticker) => {
             });
         }
     }
+    var query = pool.query("SELECT * FROM trade where is_sell IS NULL").then(function (rows, err) {
+        if (err) {
+            console.log(err);
+        }
+        for (var i in rows) {
+            var market = rows[i].MarketName;
+            var price_buy = rows[i].price_buy;
+            var id = rows[i].id;
+            markets[market]['chienluoc1']['idBuy'].push(id);
+            markets[market]['chienluoc1'].mua(price_buy);
+        }
+    });
     console.log("Price of BTC: ", ticker.BTCUSDT);
 
 });
@@ -175,25 +196,35 @@ binance.prices((error, ticker) => {
 //    console.log("balances()", balances);
 ////    console.log("ETH balance: ", balances.ETH.available);
 //});
-function setMarket(market, last = 0) {
+function setMarket(market) {
     markets[market] = {
         MarketName: market,
-        last: last,
+        last: 0,
+        volume: 0,
+        numTrades: 0,
         chienluoc1: {
             MarketName: market,
             countbuy: 2,
             notbuyinsession: false,
             amountbuy: 0.005,
             countIgnoreSession: 5,
-            minGain: 2,
+            minGain: 1,
             maxGain: 10,
             isBuy: false,
             priceBuy: [],
+            idBuy: [],
             priceBuyAvg: 0,
             checkban: function (price, candles) {
 
                 var self = this;
                 if (self.isBuy && self.priceBuyAvg > 0) {
+                    var percent = (price - self.priceBuyAvg) / self.priceBuyAvg * 100;
+                    if (percent > 0) {
+                        var textpercent = clc.green(percent.toFixed(2));
+                    } else {
+                        var textpercent = clc.red(percent.toFixed(2));
+                    }
+                    console.log(clc.black.bgWhite(self.MarketName), " price:" + price + " - " + textpercent + "%");
                     var array = Object.keys(candles);
                     var key1 = array[array.length - 3];
                     var key2 = array[array.length - 2];
@@ -208,6 +239,10 @@ function setMarket(market, last = 0) {
                      * DK 1 min < price < max
                      * DK 2 tang lien tiep 2 dot.(Xu huong tang)
                      */
+                    console.log(moment.unix(key1 / 1000).format());
+                    console.log(candle1);
+                    console.log(moment.unix(key2 / 1000).format());
+                    console.log(candle2);
                     if (price > self.minPriceSell && price < self.maxPriceSell) {
                         if (candle1.volume < candle2.volume && candle2.open < candle2.close)
                             return;
@@ -218,12 +253,14 @@ function setMarket(market, last = 0) {
             ban: function (price) {
                 var self = this;
                 console.log(clc.bgRed('Sell'), self.MarketName + " price:" + price);
-
-                var row = {
-                    MarketName: self.MarketName,
-                    price_sell: price
-                };
-                pool.query('INSERT INTO trade SET ?', row);
+                if (self.idBuy.length) {
+                    var update = {
+                        is_sell: 1,
+                        timestamp_sell: moment().format("YYYY-MM-DD HH:mm:ss.SSS")
+                    };
+                    pool.query("UPDATE trade SET ? WHERE id IN(" + Math.max(self.idBuy) + " )", {price_sell: price});
+                    pool.query("UPDATE trade SET ? WHERE id IN(" + self.idBuy.join(",") + " )", update);
+                }
                 /*
                  * RESET
                  */
@@ -232,57 +269,62 @@ function setMarket(market, last = 0) {
                 self.notbuyinsession = false;
                 self.countIgnoreSession = 5;
                 self.priceBuy = [];
+                self.idBuy = [];
                 self.priceBuyAvg = 0;
             },
             checkmua: function (price) {
                 var self = this;
                 var MarketName = self.MarketName;
+                if (!markets[MarketName] || MarketName == "BTCUSDT")
+                    return;
+
+//    console.log(markets[MarketName]);
+                if (markets[MarketName]['mfi'] < 30) {
+//        console.log(clc.black.bgYellow('Down'), MarketName + " MFI:" + markets[MarketName]['mfi']);
+                    return;
+                }
+                if (markets['BTCUSDT']['mfi'] < 30) {
+                    console.log(clc.black.bgYellow('Down'), " MFI:" + markets['BTCUSDT']['mfi']);
+                    return;
+                }
+                if (markets[MarketName]['volume'] < 1000) {
+                    return;
+                }
+
                 if (!self.notbuyinsession && self.countbuy > 0 && markets[MarketName]['rsi'] < 30 && markets[MarketName]['bb'].lower > price) {
-                    self.countbuy--;
-                    self.notbuyinsession = true;
-                    self.isBuy = true;
                     self.mua(price);
+                    var row = {
+                        MarketName: self.MarketName,
+                        price_buy: price,
+                        timestamp_buy: moment().format("YYYY-MM-DD HH:mm:ss.SSS")
+                    };
+                    pool.query('INSERT INTO trade SET ?', row).then(function (result) {
+                        self['idBuy'].push(result.insertId);
+                        var html = "<p>" + self.MarketName + "</p><p>Current Price: " + price + "</p><pre>" + JSON.stringify(markets[self.MarketName], undefined, 2) + "</pre>";
+                        mailOptions['html'] = html;
+                        transporter.sendMail(mailOptions, function (error, info) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                console.log('Email sent: ' + info.response);
+                            }
+                        });
+                    });
                 }
             },
             mua: function (price) {
                 var self = this;
-                var row = {
-                    MarketName: self.MarketName,
-                    price_buy: price
-                };
-                pool.query('INSERT INTO trade SET ?', row);
+                self.countbuy--;
+                self.notbuyinsession = true;
+                self.isBuy = true;
+
                 self['priceBuy'].push(price);
                 self['priceBuyAvg'] = math.mean(self['priceBuy']);
                 self['minPriceSell'] = self['priceBuyAvg'] + (self['priceBuyAvg'] * self['minGain'] / 100);
                 self['maxPriceSell'] = self['priceBuyAvg'] + (self['priceBuyAvg'] * self['maxGain'] / 100);
                 console.log(clc.bgGreen('Buy'), self.MarketName + " price:" + price);
                 console.log(markets[self.MarketName]);
-
-
-                var html = "<p>" + self.MarketName + " đang tăng mạnh!</p><p>Current Price: " + price + "</p><pre>" + JSON.stringify(markets[self.MarketName], undefined, 2) + "</pre>";
-                mailOptions['html'] = html;
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-                    }
-                });
             }
         }
     };
-}
-function checkPrice(MarketName, price) {
-    if (!markets[MarketName])
-        return;
-
-//    console.log(markets[MarketName]);
-    if (markets[MarketName]['mfi'] < 30) {
-//        console.log(clc.black.bgYellow('Down'), MarketName + " MFI:" + markets[MarketName]['mfi']);
-        return;
-    }
-    if (markets['BTCUSDT']['mfi'] < 30) {
-        return;
-    }
-    markets[MarketName]['chienluoc1'].checkmua(price);
 }
