77731ace9b1e532632a2d0f4d087842f182051df
DESKTOP-073RIC6\daotran <lytranuit@gmail.com>
2018-04-02 22:17:24 +0700

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const moment = require('moment');
 const binance = require('node-binance-api');
 var router = express.Router();
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
     var sumBTC = 0;
     var lastBTC = 0;
     var available = 0;
@@ -25,8 +26,37 @@ router.get('/', function (req, res, next) {
         }
     }
     var sumUSDT = sumBTC * lastBTC;
-
-    res.render('index', {title: 'Express', sumBTC: sumBTC, sumUSDT: sumUSDT, available: available, marketBuy: marketBuy, marketGood: marketGood, binance: binance});
+    /*
+     * HISTORY
+     */
+    var rows = await pool.query("SELECT *,ROUND(100 * (price_sell-price_buy) / price_buy,2) as percent FROM trade where is_sell = 1 and deleted = 0").then(function (rows, err) {
+        if (err) {
+            console.log(err);
+        }
+        var data = [];
+        for (var i in rows) {
+            var market = rows[i].MarketName;
+            var price_buy = rows[i].price_buy;
+            var price_sell = rows[i].price_sell;
+            var time_buy = moment(rows[i].timestamp_buy).format("YYYY-MM-DD HH:mm:ss");
+            var time_sell = moment(rows[i].timestamp_buy).format("YYYY-MM-DD HH:mm:ss");
+            var amount = rows[i].amount;
+            var id = rows[i].id;
+            var percent = rows[i].percent;
+            data.push({
+                market: market,
+                price_buy: price_buy,
+                price_sell: price_sell,
+                time_buy: time_buy,
+                time_sell: time_sell,
+                amount: amount,
+                percent: percent,
+                id: id
+            });
+        }
+        return data;
+    });
+    res.render('index', {title: 'Express', sumBTC: sumBTC, sumUSDT: sumUSDT, available: available, marketBuy: marketBuy, marketGood: marketGood, binance: binance, rows: rows});
 });
 router.get('/market', function (req, res, next) {
     var symbol = req.query.symbol || "BTCUSDT";
