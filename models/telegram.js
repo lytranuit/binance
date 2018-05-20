30132c7b2a4a5d8a84f6c2d46e4343a5f98ffa0c
daotran <lytranuit@gmail.com>
2018-05-20 21:08:50 +0700

    no message

--- models/telegram.js
+++ models/telegram.js
@@ -1,9 +1,14 @@
-
-const chat_id = -313318123;
-const TelegramBot = require('node-telegram-bot-api');
-const token = '606461497:AAH68TUT1mB3adaIxlud48-r-7fi2vADkRU';
-const bot = new TelegramBot(token, {polling: true});
-var send = function(html){
-	bot.sendMessage(chat_id, html,{parse_mode : "HTML"});
+if(process.env.NODE_ENV === 'production'){
+	const chat_id = -313318123;
+	const TelegramBot = require('node-telegram-bot-api');
+	const token = process.env.tokenTelegram;
+	const bot = new TelegramBot(token, {polling: true});
+	var send = function(html){
+		bot.sendMessage(chat_id, html,{parse_mode : "HTML"});
+	}
+}else{
+	var send = function(html){
+		console.log(html);
+	}
 }
 module.exports.send = send;
